f4a20703ea53c127932e806cb6b96ce2aecc1bc7
rraykzu1 <100307872+rraykzu1@users.noreply.github.com>
2022-04-16 20:54:20 -0400

    infinite rare candy, + map changes

--- src/party_menu.c
+++ src/party_menu.c
@@ -5131,7 +5131,6 @@ void ItemUseCB_RareCandy(u8 taskId, TaskFunc task)
         gPartyMenuUseExitCallback = TRUE;
         PlayFanfareByFanfareNum(FANFARE_LEVEL_UP);
         UpdateMonDisplayInfoAfterRareCandy(gPartyMenu.slotId, mon);
-        RemoveBagItem(gSpecialVar_ItemId, 1);
         GetMonNickname(mon, gStringVar1);
         ConvertIntToDecimalStringN(gStringVar2, GetMonData(mon, MON_DATA_LEVEL), STR_CONV_MODE_LEFT_ALIGN, 3);
         StringExpandPlaceholders(gStringVar4, gText_PkmnElevatedToLvVar2);
