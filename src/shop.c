594e7870ba0ab45449948930fe9d03e641450171
rraykzu1 <100307872+rraykzu1@users.noreply.github.com>
2022-04-18 10:59:11 -0400

    added item description header (credits to AsparagusEduardo for reviving it, and credits to ghoulslash for actually making it)

--- src/shop.c
+++ src/shop.c
@@ -1063,6 +1063,7 @@ static void BuyMenuTryMakePurchase(u8 taskId)
     {
         if (AddBagItem(tItemId, tItemCount) == TRUE)
         {
+            GetSetItemObtained(tItemId, FLAG_SET_OBTAINED);
             BuyMenuDisplayMessage(taskId, gText_HereYouGoThankYou, BuyMenuSubtractMoney);
             RecordItemPurchase(taskId);
         }
