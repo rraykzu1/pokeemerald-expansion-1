3c561b618b1b1585c3b65069f3cd98706d0b3ea9
rraykzu1 <100307872+rraykzu1@users.noreply.github.com>
2022-05-29 21:53:15 -0400

    Revert "PC in start menu"
    This reverts commit b1e63c7ac622974d21d113e75d8d42d39e4525a1.

--- src/strings.c
+++ src/strings.c
@@ -1966,6 +1966,3 @@ const u8 gText_FORMS_Buttons_Decapped_PE[]          = _("{START_BUTTON}Evos");
 const u8 gText_FORMS_Buttons_Submenu_PE[]           = _("{DPAD_NONE}FORMs {A_BUTTON}CHECK {START_BUTTON}EVOs");
 const u8 gText_FORMS_Buttons_Submenu_Decapped_PE[]  = _("{START_BUTTON}Evos");
 const u8 gText_FORMS_NONE[]                         = _("{STR_VAR_1} has no alternate forms.");
-
-const u8 gText_MenuPC[] = _("PC");
-
